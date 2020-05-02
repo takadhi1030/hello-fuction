ded203522149c40de145ab7ae9ab2315209c5122
takashi <tk19490612@gmail.com>
2020-05-02 20:17:27 +0900

    3テーブル分の割り勘

---
+++ warikan.py
@@ -0,0 +1,14 @@
+# Table1:1500円で3人
+amount = 1500
+number_of_people = 3
+print(f"1人あたり: {amount // number_of_people}円,端数:{amount % number_of_people}円")
+
+# Taabl2:2000円で3人
+amount = 2000
+number_of_people = 3
+print(f"1人あたり: {amount // number_of_people}円,端数:{amount % number_of_people}円")
+
+# Table3:3647円で4人
+amount = 3647
+number_of_people = 4
+print(f"1人あたり: {amount // number_of_people}円,端数:{amount % number_of_people}円")
