000b8a5a7f49c0beb98cc7b2adc6f1181b7dd1c5
takashi <tk19490612@gmail.com>
2020-05-03 09:26:04 +0900

    wairikan2からインポートしてみた

---
+++ app2.py
@@ -0,0 +1,4 @@
+# 割り勘計算したい 7000円で6人
+from warikan2 import warikan
+
+print(warikan(amount=7000, number_of_people=6))
--- warikan2.py
+++ warikan2.py
@@ -3,8 +3,7 @@ def warikan(amount, number_of_people):
     remainber = amount % number_of_people
     return f"1人あたり: {quotient}円,端数:{remainber}円"
 
-
-print(warikan(amount=1500, number_of_people=3))
-print(warikan(amount=2000, number_of_people=3))
-print(warikan(amount=3647, number_of_people=4))
-print(warikan(amount=5000, number_of_people=8))
+# print(warikan(amount=1500, number_of_people=3))
+# print(warikan(amount=2000, number_of_people=3))
+# print(warikan(amount=3647, number_of_people=4))
+# print(warikan(amount=5000, number_of_people=8))
