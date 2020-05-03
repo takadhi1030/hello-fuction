f1f5125d422b50b7284c64b34b404d6b4aabb5a5
takashi <tk19490612@gmail.com>
2020-05-03 09:13:28 +0900

    リファクタリングしてみた

--- warikan2.py
+++ warikan2.py
@@ -1,7 +1,10 @@
 def warikan(amount, number_of_people):
-    return f"1人あたり: {amount // number_of_people}円,端数:{amount % number_of_people}円"
+    quotient = amount // number_of_people
+    remainber = amount % number_of_people
+    return f"1人あたり: {quotient}円,端数:{remainber}円"
 
 
 print(warikan(amount=1500, number_of_people=3))
 print(warikan(amount=2000, number_of_people=3))
 print(warikan(amount=3647, number_of_people=4))
+print(warikan(amount=5000, number_of_people=8))
